2854fc58ef99383dea7e90f85f3c3c3e4f7ad503
Ricartho Franck <ricartho.franck@esih.edu>
2019-07-09 06:27:35 +0000

    basic login/sign up complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,14 @@ class UsersController < ApplicationController
   end
 
   post "/signup" do
-    user = User.new(:email => params[:email],:password => params[:password])
+    user = User.new(:email => params[:email],:password => params[:password],:pseudo => params[:pseudo])
     if user.save #verify whether the new user is create
       session[:user_id] = user.id
       #redirect to
       "sucees"
     else
-     redirect to "/signup"
+    #  redirect to "/signup"
+     "failed"
     end
 
   end
@@ -18,17 +19,17 @@ class UsersController < ApplicationController
       erb "/users/login".to_sym,:layout => "/layout1".to_sym
     end
 
-    # post "/login" do
-    #   user = User.find_by(:username => params[:pseudo])
-    #    if user && user.authenticate(params[:password])
-    #      session[:user_id] = user.id
-    #      #redirect to
-    #      "succes"
-    #    else
-    #      #redirect to "/login"
-    #      "failed"
-    #    end
-    # end
+    post "/login" do
+      user = User.find_by(:pseudo => params[:pseudo])
+       if user && user.authenticate(params[:password])
+         session[:user_id] = user.id
+         #redirect to
+         "succes"
+       else
+         #redirect to "/login"
+         "failed"
+       end
+    end
 
     # get "/logout" do
     #   if Helpers.is_logged_in?(session)
