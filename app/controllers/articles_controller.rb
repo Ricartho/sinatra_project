bfe4943605ea70ac6f3296ee20353acff60cb370
Ricartho Franck <ricartho.franck@esih.edu>
2019-07-11 06:08:31 +0000

    list,edit and delete in articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,4 +35,51 @@ class ArticlesController < ApplicationController
         redirect to "/login"
       end
     end
+
+    get "/articles/:id" do
+      if Helpers.is_logged_in?(session)
+        @article = Article.find(params[:id])
+        @user = User.find(session[:user_id])
+        erb "/articles/show".to_sym
+      else
+        redirect to "/login"
+      end
+    end
+
+    get "/articles/:id/edit" do
+      if Helpers.is_logged_in?(session)
+        @article = Article.find(params[:id])
+        @user = User.find(session[:user_id])
+        @categories = Category.all
+        @authors = Author.all
+        erb "/articles/edit".to_sym
+      else
+        redirect to "/login"
+      end
+    end
+
+    patch "/articles/:id" do
+      if Helpers.is_logged_in?(session)
+        @article = Article.find(params[:id])
+        @user = User.find(session[:user_id])
+        if @article.update(params[:article])
+          redirect to "/articles"
+        else
+          ##deal with validations
+        end
+      else
+        redirect to "/login"
+      end
+    end
+
+    delete "/articles/:id/delete" do
+      if Helpers.is_logged_in?(session)
+        @user = User.find(session[:user_id])
+        @article = Article.find(params[:id])
+        @article.delete
+        redirect to "/articles"
+      else
+        redirect to "/login"
+      end
+    end
 end
